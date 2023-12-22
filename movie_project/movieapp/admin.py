b3b2a20eae85e03e8a0786d64ee2c50e43def34c
unknown <manuramaiah9696@gmail.com>
2023-12-22 10:28:59 +0530

    added

---
+++ movie_project/movieapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Movie
+# Register your models her
+admin.site.register(Movie)
---
+++ movie_project/movieapp/urls.py
@@ -0,0 +1,1 @@
+from django.urls import pathfrom . import viewsapp_name='movieapp'urlpatterns = [    path('',views.home,name='home'),    path('movie/<int:movie_id>/',views.details,name='details'),    path('add/',views.add_movie,name='add_movie'),    path('update/<int:id>/',views.update,name='update'),    path('delete/<int:id>/',views.delete,name='delete'),]
